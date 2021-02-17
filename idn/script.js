4ceed1e1d8b17f44f740ccb7b622774b8a55e60a
kabilan1290 <tamiltuts1290@gmail.com>
2021-02-17 05:55:26 +0530

    IDN

---
+++ idn/script.js
@@ -0,0 +1,41 @@
+function test(){
+  var x = document.getElementById('search').value;
+  document.getElementById("demo").innerHTML = x;
+
+}
+
+function test2(){
+	var a = ['ɑ','Α','α'][Math.floor(Math.random() * 3)];
+
+	var b = ['Ь','β'][Math.floor(Math.random() * 3)];
+
+	var c="Ϲ"
+
+	var d=['ԁ','ժ']
+
+    var obj ={'a':'ɑ','b':'Ь','ԁ':'ժ','c':"Ϲ","e":"Е","f":"Ϝ","g":"ɢ","h":"Η","i":"і","j":"ј","k":"κ","l":"ι","m":"Ϻ","n":"ɴ","o":"ο","p":"ρ","q":"Ⴓ",}
+
+
+  	var x = document.getElementById('search').value;
+ 	var final ="";	
+
+ 	 for (var i = 0; i < x.length; i++) {
+ 	 	var temp = x;
+
+ 	 	if (x.charAt(i) in obj){
+ 	 			n = obj[x.charAt(i)]
+ 	 			var_i = x.charAt(i)
+ 	 			t=x.replace(var_i,n);
+ 	 			final =final+t+("\n")+("\n")
+
+	 	 	}
+	
+		}
+
+
+
+	
+	swal({
+  text: final
+});
+}
